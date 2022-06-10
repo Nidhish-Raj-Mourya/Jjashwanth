ea3908e64855c167d505886eae2ecd6d33bbd6e1
Nidhish Raj Mourya <nidhishindoremp@gmail.com>
2022-06-10 17:49:43 +0530

    first commit

---
+++ Hritik_Sharma_Sql.sql
@@ -0,0 +1,5 @@
+use ap;
+-- Solution 3:
+UPDATE vendor_address SET vendor_address1 = '1990 Westwood Blvd',  vendor_address2 = 'Ste 260' WHERE vendor_id = 4;
+
+SELECT * FROM Vendor_Address WHERE Vendor_ID = 4
